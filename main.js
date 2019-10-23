e52d2bd3cbd374068cddbcb1c12cd1a3186ad004
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-23 14:52:13 -0400

    repeat

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 function slice(string, start = 0, end = string.length) {
+  let newString = '';
+  for (let i = start; i < end; i++) {
+   newString = newString + string[i]
+  } 
+    
+  return newString 
 }
-
 function repeat(str, repetitions) {
+  let newStr = '';
+  for (let i = 0; i < repetitions; i++) {
+    newStr = newStr + str;
+  }
+
+  return newStr
 }
 
 function startsWith(string, substring) {
+  let newString = '';
+  for (let i = 0; i < substring.length; i++) {
+    newString = newString + string[i].length
+
+  }
+  return newString
 }
 
 function endsWith(str, substring) {
